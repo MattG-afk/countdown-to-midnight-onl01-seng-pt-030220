3bcc1aba830b6dbdfcd64e7cf82543f842494511
Matt Gordon <bigmattsize21@gmail.com>
2020-03-07 05:56:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,13 @@ return "Happy New Year!"
 end
 countdown 
 
+def countdown_with_sleep
+  number = 10
+  while number >= 0
+  puts "#{number} SECOND(S)!"
+  sleep(1)
+  number -= 1
+end
+return "HAPPY NEW YEAR!"
+end
+countdown_with_sleep
