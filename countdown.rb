71915c781e62cd9095c66a75b6f43e0046c7a286
Matt Gordon <bigmattsize21@gmail.com>
2020-03-07 06:06:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,6 @@
 def countdown(number)
   
-  while number >= 0
+  while number > 0
   puts "#{number} SECOND(S)!"
   number -= 1
 end
@@ -10,7 +10,7 @@ countdown(10)
 
 def countdown_with_sleep(number)
 
-  while number >= 0
+  while number > 0
   puts "#{number} SECOND(S)!"
   sleep(1)
   number -= 1
